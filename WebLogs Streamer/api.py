5144f6fcbebc73e7e6bda373395151c81ac635be
ayadirh <hridayasagarsubedi@gmail.com>
2020-06-16 22:00:48 +0200

    stream updated with instacart productid for itemid

--- WebLogs Streamer/api.py
+++ WebLogs Streamer/api.py
@@ -2,17 +2,36 @@ import flask
 import pandas as pd
 from flask import request, jsonify, stream_with_context, Response, render_template
 import time,datetime
+#import math,json, random
+from random import randrange
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 # Reading Events.csv from RetailRocket files
 filepath= r"C:/Users/ayadi/Downloads/events.csv"
+productslist = r"C:/Users/ayadi/Downloads/4931_7487_bundle_archive/products.csv"
+productread = pd.read_csv(productslist, usecols = ['product_id'], error_bad_lines = False)
 dataread = pd.read_csv(filepath, usecols = ['timestamp','visitorid','event','itemid', 'transactionid'], error_bad_lines = False)
 print('--reading--', filepath, '| Empty:',dataread.empty)
 
+'''
 def getData(index):
+    temp = json.loads(dataread.iloc[[index]].to_json(orient='records'))
+    if dataread.iloc[[index]]["event"].item() == 'view':
+        for i in temp:
+           i["loggedIn"] = bool(random.getrandbits(1))
+    else:
+        for i in temp:
+            i["loggedIn"] = True
+    print(index, temp)
+    return (json.dumps(temp))
+'''
+
+def getData(index):
+    dataread.at[index,'itemid'] = productread.iloc[[randrange(len(productread.index))]]['product_id']
     return (dataread.iloc[[index]].to_json(orient='records'))
+
 
 @app.route('/', methods=['GET'])
 def home():
@@ -26,7 +45,7 @@ def getWebLogs():
         count = 0;
         while True:
             yield getData(count)
-            time.sleep(.1)
+            time.sleep(.01)
             count +=1
     return Response(generate(), mimetype='text/json')
 
