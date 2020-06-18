40e6d38a467ad69d71c51cc15207f6732a8cad2f
ayadirh <hridayasagarsubedi@gmail.com>
2020-06-18 11:13:24 +0200

    ratings added to products.csv

--- Add Ratings/addRatings.py
+++ Add Ratings/addRatings.py
@@ -4,15 +4,18 @@ import math, random
 
 
 # Reading orders.csv from Instacart files
-orderslist = r"C:/Users/ayadi/Downloads/4931_7487_bundle_archive/orders.csv"
+#orderslist = r"C:/Users/ayadi/Downloads/4931_7487_bundle_archive/orders.csv"
+# Reading products.csv from Instacart files
+productslist = r"C:/Users/ayadi/Downloads/4931_7487_bundle_archive/products.csv"
 
-orderread = pd.read_csv(orderslist, usecols = ['order_id','user_id','eval_set','order_number','order_dow','order_hour_of_day','days_since_prior_order' ], error_bad_lines = False)
-print('Length: ', len(orderread.index))
+#orderread = pd.read_csv(orderslist, usecols = ['order_id','user_id','eval_set','order_number','order_dow','order_hour_of_day','days_since_prior_order' ], error_bad_lines = False)
+productread = pd.read_csv(productslist, usecols = ['product_id','product_name','aisle_id','department_id'], error_bad_lines = False)
+print('Length: ', len(productread.index))
 
 ratinglist = [1,2,3,4,5]
 ratings = []
-for i in range(len(orderread.index)):
+for i in range(len(productread.index)):
     ratings.append(random.choice(ratinglist))
 
-orderread['ratings'] = ratings
-orderread.to_csv( 'ratedOrders.csv', sep=',', index=False)
+productread['ratings'] = ratings
+productread.to_csv( 'ratedProducts.csv', sep=',', index=False)
